29d05ba397d5b766e51c9cc1ebf638540738354f
Martin Wåger <martin.w@comaround.se>
2015-11-06 13:36:25 +0100

    base

---
+++ TodoListWebApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace TodoListWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Error(string message)
+		{
+			ViewBag.Message = message;
+			return View("Error");
+		}
+	}
+}
---
+++ TodoListWebApp/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TodoListWebApp.Startup))]
+
+namespace TodoListWebApp
+{
+	public partial class Startup
+	{
+		// ReSharper disable once UnusedMember.Global
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
